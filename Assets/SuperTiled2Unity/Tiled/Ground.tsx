e5e686f9ccc20773c9d27d78d3ad95b390164dc8
Khanh <kaishinigami123@gmail.com>
2024-10-12 22:06:06 +0700

    New map

---
+++ Assets/SuperTiled2Unity/Tiled/Ground.tsx
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Ground" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+ <image source="Tiles/Assets/Assets.png" width="400" height="400"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0.03125">
+    <polygon points="0,0 14.9844,-0.015625 15,16 0.03125,15.9844 0.0625,11.9844 0.953125,11.9688 1,7.98438 0.015625,8.01563"/>
+   </object>
+   <object id="2" x="1" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.04348" y="0.173913">
+    <polygon points="0,0 -0.0434783,4.82609 4.04348,4.78261 3.95652,6.04348 2.95652,5.78261 2.95652,7.30435 2.91304,7.86957 5,7.82609 4.95652,15.8261 9,15.8696 8.95652,7.91304 14.9565,7.86957 14.9565,-0.173913"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" x="0.0869565" y="0.26087">
+    <polygon points="0,0 0.0869565,7.73913 15.9565,7.65217 15.7826,-0.217391"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="131">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0.0888889">
+    <polygon points="0,0 -0.0222222,4.86667 4.04444,4.93333 4,5.86667 3.04444,5.91111 3.02222,7.93333 15,7.86667 14.9778,-0.133333"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
