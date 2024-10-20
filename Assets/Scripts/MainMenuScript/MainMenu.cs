64affebba51425aa577de381637cff3e363a6740
Tien Hieu <122421124+tienhieu03@users.noreply.github.com>
2024-10-21 01:42:58 +0700

    add sound setting

--- Assets/Scripts/MainMenuScript/MainMenu.cs
+++ Assets/Scripts/MainMenuScript/MainMenu.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
  
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    
     private void Start()
     {
+        LoadVolume();
         MusicManager.Instance.PlayMusic("MainMenu");
     }
     public void Play()
@@ -14,14 +21,33 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("GameScene");
         MusicManager.Instance.PlayMusic("Game");
     }
+    public void Quit()
+    {
+        Application.Quit();
+    }
     public void GameMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
-
-    public void Quit()
+    public void UpdateMusicVolume(float volume)
     {
-        Application.Quit();
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
  
