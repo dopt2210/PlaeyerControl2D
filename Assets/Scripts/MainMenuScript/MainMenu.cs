d17439c5ee4a386e929a81f574a9a45ba9fb8089
Tien Hieu <122421124+tienhieu03@users.noreply.github.com>
2024-10-20 23:56:31 +0700

    update mainmenu/game music

--- Assets/Scripts/MainMenuScript/MainMenu.cs
+++ Assets/Scripts/MainMenuScript/MainMenu.cs
@@ -5,9 +5,14 @@ using UnityEngine.SceneManagement;
  
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        MusicManager.Instance.PlayMusic("MainMenu");
+    }
     public void Play()
     {
         SceneManager.LoadScene("GameScene");
+        MusicManager.Instance.PlayMusic("Game");
     }
     public void GameMenu()
     {
