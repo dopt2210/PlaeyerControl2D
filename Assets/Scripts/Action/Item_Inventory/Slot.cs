3c8fc73179c1f81b480d0ddd3b2aeb7b95c46749
Tien Hieu <122421124+tienhieu03@users.noreply.github.com>
2024-11-08 17:36:55 +0700

    press G to drop

--- Assets/Scripts/Action/Item_Inventory/Slot.cs
+++ Assets/Scripts/Action/Item_Inventory/Slot.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
+    public static Slot selectedSlot;
+    public int slotNumber;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SelectSlot(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SelectSlot(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SelectSlot(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G) && selectedSlot == this)
+        {
+            DropItem();
+        }
+    }
+
+    void SelectSlot(int number)
+    {
+        if (slotNumber == number)
+        {
+            selectedSlot = this;
+        }
+    }
+
     public void DropItem()
     {
         foreach (Transform child in transform)
