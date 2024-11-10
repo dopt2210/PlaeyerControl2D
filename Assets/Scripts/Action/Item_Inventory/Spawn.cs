f251a04f9da4400e42c077b6d7e57fee9aabcb25
Tien Hieu <122421124+tienhieu03@users.noreply.github.com>
2024-11-10 23:36:38 +0700

    Revert "Hieu UI"

--- Assets/Scripts/Action/Item_Inventory/Spawn.cs
+++ Assets/Scripts/Action/Item_Inventory/Spawn.cs
@@ -13,7 +13,7 @@ public class Spawn : MonoBehaviour
     }
     public void SpawnDroppedItem()
     {
-        Vector2 playerPos = new Vector2(player.position.x + 3, player.position.y);
+        Vector2 playerPos = new Vector2(player.position.x, player.position.y);
         Instantiate(item, playerPos, Quaternion.identity);
     }
 }
