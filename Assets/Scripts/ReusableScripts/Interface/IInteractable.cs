3ebb570c857d0b3c5cb5dc9997b5781a9ed11b2a
Dopt Aki <duckun203@gmail.com>
2024-11-24 03:34:20 +0700

    fix some bug

--- Assets/Scripts/ReusableScripts/Interface/IInteractable.cs
+++ Assets/Scripts/ReusableScripts/Interface/IInteractable.cs
@@ -7,4 +7,6 @@ public interface IInteractable
     GameObject PlayerGameObject { get; set; }
     bool IsInteractable { get; set; }
     public void Interact();
+    public void DisableInteract();
+    public void SetRangeInteract(bool value);
 }
